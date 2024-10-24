1e7740a88f09feea9e7f7fa0e235ff9ee7506fb4
hoangle <hoangle@hoangg.local>
2024-10-24 09:19:15 +0700

    update 0.1.8

--- lib/updater_gf.rb
+++ lib/updater_gf.rb
@@ -9,11 +9,6 @@ module UpdaterGf
   class Updater
     class << self
       def run(add_gem = '', add_robocop = '')
-        if check_argv_error?(add_gem, add_robocop)
-          show_message_argv_error
-          return
-        end
-
         is_add_gem = add_gem?(add_gem)
         is_add_robocop = add_robocop?(add_gem, add_robocop)
         HandleUpdate.new(is_add_gem, is_add_robocop).process
@@ -25,20 +20,6 @@ module UpdaterGf
 
       def add_robocop?(add_gem, add_robocop)
         add_robocop == '-r' || add_gem == '-r'
-      end
-
-      def check_argv_error?(add_gem, add_robocop)
-        return true if (add_gem != '-a' || add_gem != '-r') && add_gem != ''
-        return true if add_robocop != '' && add_robocop != '-r'
-
-        false
-      end
-
-      def show_message_argv_error
-        puts 'Invalid arguments'
-        puts 'Usage: updater_gf [-a] [-r]'
-        puts '  -a: Add gems from list_gems_add to Gemfile'
-        puts '  -r: Add .rubocop.yml to project root'
       end
     end
   end
